5469f3d5b31126cf84302158d615dcdfb25b5456
Pedro Henrique Cordeiro <pedro.henrique.cefetmg@gmail.com>
2023-11-03 07:55:27 -0300

    Create main.tf

---
+++ aws/s3/main.tf
@@ -0,0 +1,15 @@
+# Configure the aws provider
+
+terraform {
+    required_providers{
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.0"
+        }
+    }
+}
+
+
+provider "aws" {
+    region = "us-east-2"
+}
